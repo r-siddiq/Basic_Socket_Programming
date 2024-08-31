77e6dc0999d1a068fc075260bfe66fda1b7dec48
R-Siddiq <roshsiddiq@gmail.com>
2024-08-30 22:23:52 -0700

    Step 3 complete

--- udp_client.py
+++ udp_client.py
@@ -1,7 +1,7 @@
 import socket
 
-HOST = ""  # todo: specify the server's hostname or IP address inside the quotes
-PORT =  # todo: specify the port number used by the server
+HOST = "127.0.0.1"  # todo: specify the server's hostname or IP address inside the quotes
+PORT = 12345 # todo: specify the port number used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.connect((HOST, PORT))
