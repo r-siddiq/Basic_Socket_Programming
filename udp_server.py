45cf393256592f129516f4e8386bd0b62e59661b
R-Siddiq <roshsiddiq@gmail.com>
2024-08-31 00:34:13 -0700

    s.recvfrom was using PORT

--- udp_server.py
+++ udp_server.py
@@ -8,7 +8,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
     print('Server listening on {}:{}'.format(HOST, PORT))
     while True:
-        data, addr = s.recvfrom(PORT)
+        data, addr = s.recvfrom(1024)
         if not data:
             break
         s.sendto(data.upper(), addr)
